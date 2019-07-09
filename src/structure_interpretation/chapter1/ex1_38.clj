c056f67dfc972bf350e618d0aca707038d7915ff
Simion Belea <simion.belea@rvu.co.uk>
2019-07-09 20:58:42 +0100

    cleanup

--- src/structure_interpretation/chapter1/ex1_38.clj
+++ src/structure_interpretation/chapter1/ex1_38.clj
@@ -1,35 +1,11 @@
 (ns chapter1.ex1_38)
 
-(def phi-value 1.608339)
-
 (defn cont-frac [n d k]
   (loop [k k
          acc 0]
     (if (< k 1) acc
         (recur (dec k) (/ (n k) (+ (d k) acc))))))
 
-(cont-frac
- (constantly 1.0)
- (constantly 1.0)
- 16)
-(defn euler-denom [i]
-  (let [x (dec i)]
-    (if (or (= 0 (mod x 3)))
-      (* 2 (inc (/ x 3)))
-      1.0)))
-
-(euler-denom 0)
-;; => 1.0
-(euler-denom 1)
-;; => 2
-(euler-denom 2)
-;; => 1.0
-(euler-denom 3)
-;; => 1.0
-(euler-denom 4)
-;; => 4
-(- Math/E 2)
-;; => 0.7182818284590451
 (defn d [i]
   (let [i (dec i)
         idx (mod i 3)]
