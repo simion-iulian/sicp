aec1084125e24b2fa94c476f79171f7f7a764642
Simion Belea <simion.belea@rvu.co.uk>
2019-08-07 23:39:25 +0100

    Incomplete naive implementation for perimeter and area - needs to work for all possible sides

--- src/structure_interpretation/chapter2/chapter2.clj
+++ src/structure_interpretation/chapter2/chapter2.clj
@@ -97,3 +97,54 @@
  (midpoint 
   (make-segment (make-point 1 1)
                 (make-point 3 3))))
+
+;Exercise 2.3
+;Implement a representation for rectangles in a plane
+;Constructors and selectors for rectangles
+;Procedures for area given a rectangle, and for the peremiter
+;Now implement a different representation for rectangles
+;Can you design your system with suitable abstraction barriers?
+;So that the same perimeter and procedures will work using either representation?
+
+
+;Do I make use only of points or of segments?
+
+;Point impelementation
+(defn make-rect 
+  [point1 point2 point3 point4]
+  (list point1 point2 point3 point4))
+
+(defn rect-point [rect x]
+  (if (<= 0 x 3)
+    (nth rect x)
+    (prn "Outside allowed boundaries")))
+
+(defn abs [n] (max n (- n)))
+
+;This works only for lines parallel to the axes, how can it work for all lines?
+(defn rect-perimeter [rect]
+  (let [twice-one-side   (* 2 (abs 
+                               (- (-> rect (rect-point 0) x-point)
+                                  (-> rect (rect-point 1) x-point))))
+        twice-other-side (* 2 (abs
+                               (- (-> rect (rect-point 1) x-point)
+                                  (-> rect (rect-point 2) x-point))))]
+    (+ twice-one-side twice-other-side)))
+
+(defn rect-area [rect]
+  (let [one-side (abs (- (-> rect (rect-point 0) x-point)
+                         (-> rect (rect-point 1) x-point)))
+        adjacent-side (abs (- (-> rect (rect-point 1) x-point)
+                              (-> rect (rect-point 2) x-point)))]
+    (* one-side adjacent-side)))
+
+;How do I ensure the points are bounded correctly?
+;This implementation is tightly bound to order of things.
+;Could the constructor `construct` the rectangle 
+;instead of just holding the data in a structure?
+(rect-perimeter 
+ (make-rect 
+  (make-point 1 1)
+  (make-point 1 3)
+  (make-point 3 3)
+  (make-point 1 1)))
