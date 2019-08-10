12eaa6adcc4eaf78ef4ae7ac4d7845c533bdf1cf
Simion Belea <simion.belea@rvu.co.uk>
2019-08-10 20:17:40 +0100

    Moved implementation to use maps instead of sets. Move exercise into its own namespace. Added test case for non-parallel rectangle

--- test/structure_interpretation/chapter2/chapter2_test.clj
+++ test/structure_interpretation/chapter2/chapter2_test.clj
@@ -1,22 +1,22 @@
 (ns chapter2-test.clj
   (:require [clojure.test :refer :all]
-            [chapter2 :refer :all]))
+            [chapter2.exercise2.2-2.3 :refer :all]))
 
-(deftest exercise2-2
+(deftest exercise2.3
   (testing "That functions for the rectangle system in exercise 2.2 behave as expected"
     (testing "That sides connected check works regardless of how the segment is constructed"
-      (is (sides-connected? (make-segment (make-point 1 1)
-                                          (make-point 1 3))
-                            (make-segment (make-point 1 3)
-                                          (make-point 2 3))))
-      (is (sides-connected? (make-segment (make-point 1 3)
-                                          (make-point 1 1))
-                            (make-segment (make-point 1 3)
-                                          (make-point 2 3)))))
+      (is (sides-connected? (make-segment (point 1 1)
+                                          (point 1 3))
+                            (make-segment (point 1 3)
+                                          (point 2 3))))
+      (is (sides-connected? (make-segment (point 1 3)
+                                          (point 1 1))
+                            (make-segment (point 1 3)
+                                          (point 2 3)))))
     (testing "That we can check there is a right angle"
       (is (right-angle? (make-segment
-                         (make-point 1 1)
-                         (make-point 1 3))
+                         (point 1 1)
+                         (point 1 3))
                         (make-segment
-                         (make-point 1 3)
-                         (make-point 2 3)))))))
+                         (point 1 3)
+                         (point 2 3)))))))
